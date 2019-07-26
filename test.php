e7c3a6bcdbd5c85d9a9bf56cb4f1ce9c8547d18c
Milos <milos@djakonovic.net>
2019-07-26 14:57:03 +0200

    test preparing

--- test.php
+++ test.php
@@ -1,40 +1,103 @@
 <?php
 
+function Check($description, $arg){
+    if (!is_callable($arg)) return false;
 
+    $additional = 'checkid_' . uniqid();
+
+
+    if(call_user_func($arg, $additional)){
+        echo "\r\n\r\nPASSED : $description\r\n";
+        if(isset($GLOBALS[$additional]))
+            echo $GLOBALS[$additional] . "\r\n";
+        return true;
+    } else {
+        echo "\r\n\r\nFAILED : $description\r\n";
+        if(isset($GLOBALS[$additional]))
+            echo $GLOBALS[$additional] . "\r\n";        
+        $GLOBALS['_Check_something_failed']=true;
+        return true;
+    }
+}
+
+register_shutdown_function(function(){
+    if(isset($GLOBALS['_Check_something_failed']) && $GLOBALS['_Check_something_failed'] === true)
+        echo "
+|----------------------------------|
+|-----There are failed tests!------|
+|----------------------------------|
+";
+        else echo "
+|----------------------------------|
+|-------ALL TESTS ARE FINE!--------|
+|----------------------------------|
+";
+});
+
+/*Check("desc",function(){
+
+});*/
 
 require('safexec.php');
 
 
+// #php pro tip: when you want to avoid name colision by prefixing var or fn name with 'custom', you are increasing the odds of colision.
+
+Check("minimal example - command without arguments (whoami)", function($additional){
+    $GLOBALS[$additional] = "simple as that";
+    return safexec('whoami') === true;
+});
 
 
-if( safexec('whoami') === true ){
-    echo "Passed - minimal example - command without arguments (whoami)\r\n";
-} else {
-    echo "Failed! - minimal example - command without arguments (whoami)\r\n";
-}
-
-
-
-if ( safexec('whoami', $output1) ){
-    //if ($output1)
-    exec('whoami', $o1, $s1);
-    if( implode( PHP_EOL, $o1 ) === $output1 )
-        echo "Passed - example with catching output - command without arguments (whoami)\r\n";
-    else 
-        echo "Failed! - example with catching output - outputs doesn't match - command without arguments (whoami)\r\n";
-} else echo "Failed! - example with catching output - command itself failed - command without arguments (whoami)\r\n";
-
-
-
-
-if( safexec('dig %s', $output2, array('example.com')) ){
-    if( is_string($output2) && preg_match('/Got answer\:.*ANSWER SECTION\:/ms', $output2) ){
-        echo "Passed - command dig with passed param example.com\r\n";
+Check("example with catching output - command without arguments (whoami)", function($a){
+    if(!safexec('whoami', $output1)){
+        $GLOBALS[$a] = "command whoami itself has failed";
+        return false;
     }
+    exec('whoami', $o1, $s1);
+    if (implode( PHP_EOL, $o1 ) === $output1){
+        $GLOBALS[$a] = "both commands are good";
+        return true;
+    }
+});
+
+
+Check("command dig with passed with param example.com", function($a){
+    if(! safexec('dig %s', $output2, array('example.com')) ){
+        $GLOBALS[$a] = 'dig command itself has failed';
+    }
+    return ( is_string($output2) && preg_match('/Got answer\:.*ANSWER SECTION\:/ms', $output2) );
+});
+
+
+Check("test of injection of command echo", function($a){
+    if(safexec('php tstcmds/cmdinjectiontest.php %s', $output3, array('example.com && echo shouldnthappen'))){
+        if(!empty($output3))
+            $GLOBALS[$a] = $output3;
+        return true;
+    }
+    return false;
+});
+
+if(DIRECTORY_SEPARATOR === '/'){
+    //*nix
+   
+    Check("test of injection of command echo", function($a){
+        if(safexec('php tstcmds/cmdinjectiontest.php %s', $output3, array('example.com; echo shouldnthappen'))){
+            if(!empty($output3))
+                $GLOBALS[$a] = $output3;
+            return true;
+        }
+        return false;
+    });  
+    
+    Check("test of injection of command echo", function($a){
+        if(safexec('php tstcmds/cmdinjectiontest.php %s', $output3, array('example.com | echo "shouldnthappen"'))){
+            if(!empty($output3))
+                $GLOBALS[$a] = $output3;
+            return true;
+        }
+        return false;
+    });    
+    
 }
-
-
-
-safexec('php testcommand.php %s', $output3, array('example.com; echo shouldnthappen'));
-echo $output3;
-
---
+++ tstcmds/cmdinjectiontest.php
@@ -0,0 +1,9 @@
+<?php
+
+echo "\r\n";
+var_dump($argv);
+
+
+if(count($argv) !== 2)
+    exit(2);
+exit(0);
